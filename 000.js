590e2abe3ca196475bf28fc5056cba69c7a9d83c
Denis Danchuk <d.danchuk@astoundcommerce.com>
2016-04-05 18:17:53 +0300

    Currently scrolling images only on pressing buttons

---
+++ 000.js
@@ -0,0 +1,29 @@
+// http://stackoverflow.com/a/2335554/3841259
+var sliderWidth = 0;
+$('.jq_slider_images img').each(function() { sliderWidth += $(this).width(); });
+$('.jq_slider_images').width(sliderWidth);
+
+var sliderPosition = 0;
+var sliderLastChildWidth = $('.jq_slider_images li:last-child').width();
+
+
+
+$('.jq_slider_navigation_prev').click(function() {
+    sliderPosition = sliderPosition + 800;
+    if (sliderPosition > 0) {
+        sliderPosition = -(sliderWidth - sliderLastChildWidth);
+        $(".jq_slider_images").css('margin-left', -(sliderWidth - sliderLastChildWidth));
+    } else {
+        $(".jq_slider_images").css('margin-left', sliderPosition);
+    }
+});
+
+$('.jq_slider_navigation_next').click(function() {
+    sliderPosition = sliderPosition - 800;
+    if (sliderPosition < -(sliderWidth - sliderLastChildWidth)) {
+        sliderPosition = 0;
+        $(".jq_slider_images").css('margin-left', 0);
+    } else {
+        $(".jq_slider_images").css('margin-left', sliderPosition);
+    }
+});
