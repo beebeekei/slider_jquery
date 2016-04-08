ddf561d769abdd8d1a2ac93c7415a58a20a5e804
Denis Danchuk <d.danchuk@astoundcommerce.com>
2016-04-08 13:07:14 +0300

    Changing structure

---
+++ app.js
@@ -0,0 +1,1 @@
+slderSettings.pagination = false;
--- jq_slider.js
+++ jq_slider.js
@@ -1,4 +1,8 @@
 //$(window).load(function() {
+    var slderSettings = {
+        pagination: true
+    };
+
     // http://stackoverflow.com/a/2335554/3841259
     var sliderWidth = 0;
     $('.jq_slider_images img').each(function() { sliderWidth += $(this).width(); });
@@ -7,18 +11,20 @@
     var sliderPosition = 0; //start position
     var sliderLastChildWidth = $('.jq_slider_images li:last-child').width();
 
-    var paginationCoords = [0]; //array will always contain 0 - as a start position
 
-    //making an array with margin-left coordinates for images
-    for (var i = 1; i < $('.jq_slider_images img').length; i++) {
-        paginationCoords[i] = paginationCoords[i - 1] - $($('.jq_slider_images img')[i - 1]).width();
-    }
+    if (slderSettings.pagination) {
+        var paginationCoords = [0]; //array will always contain 0 - as a start position
 
-    //adding pagination
-    for (var i = 0; i < $('.jq_slider_images img').length; i++) {
-        $('.jq_slider_pagination').append('<li><span class="jq_slider_pagination_dot">' + (i + 1) + '</span></li>');
+        //making an array with margin-left coordinates for images
+        for (var i = 1; i < $('.jq_slider_images img').length; i++) {
+            paginationCoords[i] = paginationCoords[i - 1] - $($('.jq_slider_images img')[i - 1]).width();
+        }        
+        //adding pagination
+        for (var i = 0; i < $('.jq_slider_images img').length; i++) {
+            $('.jq_slider_pagination').append('<li><span class="jq_slider_pagination_dot">' + (i + 1) + '</span></li>');
+        }
+        $($('.jq_slider_pagination_dot')[0]).addClass('active'); //initial value of first slide
     }
-    $($('.jq_slider_pagination_dot')[0]).addClass('active'); //initial value of first slide
 
     //changing slide by clicking on pagination dots
     $('.jq_slider_pagination_dot').each(function(index) {
@@ -31,6 +37,7 @@
         $(this).addClass('active');
     });
 
+    //previuos button
     $('.jq_slider_navigation_prev').click(function() {
         sliderPosition += 800;
         if (sliderPosition > 0) {
@@ -45,6 +52,7 @@
         $($('.jq_slider_pagination_dot')[paginationCoords.indexOf(sliderPosition)]).addClass('active');
     });
 
+    //next button
     $('.jq_slider_navigation_next').click(function() {
         sliderPosition -= 800;
         if (sliderPosition < -(sliderWidth - sliderLastChildWidth)) {
